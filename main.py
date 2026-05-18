c80164ebd5ba3dc6f5d4cee8ba1d8826016befa0
hshs83892259-byte <hshs83892259@gmail.com>
2026-05-18 10:03:49 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 st.title('나의 첫 웹서비스 만들기')
-st.text_input('야 기분 딱 좋다')
-st.selectbox('뭐 좋아하노',['응디','게이','똥꼬'])
-st.button('응디')
+a=st.text_input('야 기분 딱 좋다')
+b=st.selectbox('뭐 좋아하노',['응디','게이','똥꼬'])
+if st.button('응디'):
+  st.write(a+'님, 요로시쿠')
