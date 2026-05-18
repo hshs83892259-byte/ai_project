7a00fb2000a8483b2b9fd4123c38cd3f7a4b00ae
hshs83892259-byte <hshs83892259@gmail.com>
2026-05-18 10:12:30 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,7 @@ a=st.text_input('야 기분 딱 좋다')
 b=st.selectbox('뭐 좋아하노',['응디','게이','똥꼬'])
 if st.button('응디'):
   st.write(a+'님, 요로시쿠')
+st.info('하이요')
+st.warning('b+ 이것을 좋아하시나봐요')
+st.error('tlqkf')
+st.baiioons
